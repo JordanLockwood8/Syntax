6fc01d94361dd2808bee0852bdf6d8393e0eb4c8
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-07 17:47:23 -0600

    project for syntax

---
+++ Syntax/Syntax/Program.cs
@@ -0,0 +1,22 @@
+﻿//switched int to var
+var answer = 4;
+//use ctl+k+c
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+//write out my ternary operator to match above ,if else
+string response = (answer < 9) ? " is less than nine " : " is greater than or equal to nine ";
+Console.WriteLine(response);
+Console.WriteLine();
+
+string animals = "dogs";
+string name = "gus";
+int age = 3;
+var thisdog = $" this {animals} name is {name} and it is {age}";
+
